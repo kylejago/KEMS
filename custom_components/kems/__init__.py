d6bda036237d8a3315c6326dd129d6cf0461dc26
kylejago <43718551+kylejago@users.noreply.github.com>
2026-08-25 11:35:15 +0100

    Alpha8.15: activate convergent update orchestrator

--- custom_components/kems/__init__.py
+++ custom_components/kems/__init__.py
@@ -48,7 +48,7 @@ from .providers.octopus import OctopusProvider
 from .providers.ohme import OhmeProvider
 from .settings import KEMSSettings
 from .update_orchestrator import async_unload_update_orchestrator
-from .update_orchestrator_reliable import async_setup_update_orchestrator
+from .update_orchestrator_convergent import async_setup_update_orchestrator
 
 LOGGER = logging.getLogger(__name__)
 
